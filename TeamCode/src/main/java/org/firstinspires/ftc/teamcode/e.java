e24fd7f0bdc5009d854b8fbc94021f7c2dc369f2
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-08-16 14:28:58 -0500

    fdhg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/e.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class TurtleTest {
+<<<<<<< HEAD
+    public static void main(String[] args) {
+        
+    }
+}
+=======
+} 
+
+>>>>>>> 10d80766b56bc4015707d7c21063062a6ab6319b
