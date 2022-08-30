886c7c09422511b8915ff1b7b8e9e689c6818712
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-08-30 15:08:08 -0500

    Update TurtleTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
@@ -1,7 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TurtleTest {
-    public static void main(String[] args) {
+    public class TurtleTest extends LinearOpMode {
+        DcMotor motorLeft;
+        DcMotor motorRight;
+        ColorSensor color1;
+
+        @Override
+        public void runOpMode() {
+            motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+            motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+
+            // Put initialization blocks here
+            waitForStart();
+            // Put run blocks here
+            while (opModeIsActive()) {
+                motorLeft.setPower(-7);
+                motorRight.setPower(5);
+                // Put loop blocks here
+            }
+        }
 
     }
-}
+
