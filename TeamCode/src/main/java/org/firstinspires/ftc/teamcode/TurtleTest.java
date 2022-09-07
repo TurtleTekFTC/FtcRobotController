412f6c7961757040780622b1387da433d7477980
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-09-06 19:15:11 -0500

    Update TurtleTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.view.animation.GridLayoutAnimationController;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -11,7 +13,6 @@ public class TurtleTest extends LinearOpMode {
         public void runOpMode() {
             motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
             motorRight = hardwareMap.get(DcMotor.class, "motorRight");
-
             // Put initialization blocks here
             waitForStart();
             // Put run blocks here
@@ -23,6 +24,9 @@ public class TurtleTest extends LinearOpMode {
                 motorLeft.setPower(0);
                 motorRight.setPower(0);
                 sleep(500);
+                motorLeft.setPower(0.1);
+                motorRight.setPower(-0.1);
+                sleep(1000);
                 // Put loop blocks here
             }
         }
