0f3322bdc8e6db3d7482559dfe52e74919394a18
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-09-23 17:57:20 -0500

    Update TurtleTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
@@ -23,8 +23,8 @@ public class TurtleTest extends LinearOpMode {
 
                 boolean test = gamepad1.dpad_up;
                 if  (gamepad1.dpad_up) {
-                    motorLeft.setPower(1);
-                    motorRight.setPower(-1);
+                    motorLeft.setPower(0.1);
+                    motorRight.setPower(-0.1);
                 }
                 else {
                     motorLeft.setPower(0);
@@ -32,8 +32,8 @@ public class TurtleTest extends LinearOpMode {
                 }
                 boolean test2 = gamepad1.dpad_right;
                 if  (gamepad1.dpad_right) {
-                    motorLeft.setPower(1);
-                    motorRight.setPower(1);
+                    motorLeft.setPower(0.1);
+                    motorRight.setPower(0.1);
                 }
                 else {
                     motorLeft.setPower(0);
@@ -41,8 +41,8 @@ public class TurtleTest extends LinearOpMode {
                 }
                 boolean test3 = gamepad1.dpad_left;
                 if  (gamepad1.dpad_left) {
-                    motorLeft.setPower(-1);
-                    motorRight.setPower(-1);
+                    motorLeft.setPower(-0.1);
+                    motorRight.setPower(-0.1);
                 }
                 else {
                     motorLeft.setPower(0);
@@ -50,8 +50,8 @@ public class TurtleTest extends LinearOpMode {
                 }
                 boolean test4 = gamepad1.dpad_down;
                 if  (gamepad1.dpad_down) {
-                    motorLeft.setPower(-1);
-                    motorRight.setPower(1);
+                    motorLeft.setPower(-0.1);
+                    motorRight.setPower(0.1);
                 }
                 else {
                     motorLeft.setPower(0);
