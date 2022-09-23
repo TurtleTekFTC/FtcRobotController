588afc50ea878cad1a45c2769a2e25339a6b687c
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-09-23 17:53:03 -0500

    Update TurtleTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name="TurtleTestMode", group="Turtle Group")
+@TeleOp(name="TurtleTestMode", group="Turtle Group")
 public class TurtleTest extends LinearOpMode {
         DcMotor motorLeft;
         DcMotor motorRight;
