8e55c1216969d9de61662306f823d873e4a638e0
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-09-30 16:37:54 -0500

    Update DriveWithSticks.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveWithSticks.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveWithSticks.java
@@ -1,24 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.view.animation.GridLayoutAnimationController;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="DriveWithSticksJava", group="Turtle Group")
 public class DriveWithSticks extends LinearOpMode {
     DcMotor motorLeft;
     DcMotor motorRight;
     DcMotor motorArm;
+    Servo claw1;
+    Servo claw2;
 
     @Override
     public void runOpMode() {
         motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
         motorRight = hardwareMap.get(DcMotor.class, "motorRight");
         motorArm = hardwareMap.get(DcMotor.class, "motorArm");
-        Claw = hardwareMap.get(.class, "Claw");
+        claw1 = hardwareMap.get(Servo.class, "claw1");
+        claw2 = hardwareMap.get(Servo.class, "claw2");
+
 
 
         // Put initialization blocks here
@@ -29,6 +31,20 @@ public class DriveWithSticks extends LinearOpMode {
             motorLeft.setPower(gamepad1.left_stick_y);
             motorRight.setPower(gamepad1.right_stick_y);
 
+            if (gamepad2.left_stick_y);
+
+                motorArm.setPower(gamepad2.left_stick_y)
+
+            if (gamepad2.left_bumper) {
+
+                claw1.setPosition(0.25);
+
+            }
+            else if (gamepad2.right_bumper) {
+
+                claw1.setPosition(0);
+
+            }
         }
     }
 }
