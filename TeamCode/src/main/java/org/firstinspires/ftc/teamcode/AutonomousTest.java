d45578022bc1dc390e9ddefc07fa92bf7eca26c0
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-09-30 19:07:39 -0500

    Switched to using RobotHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -5,42 +5,31 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware;
+
 @Autonomous(name="TurtleTestAutonomous", group="Turtle Group")
 public class AutonomousTest extends LinearOpMode{
-    DcMotor motorLeft;
-    DcMotor motorRight;
-    DcMotor motorArm;
-    Servo claw1;
-    Servo claw2;
+    RobotHardware_TT   robot       = new RobotHardware_TT(this);
 
     @Override
     public void runOpMode() {
-        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
-        motorArm = hardwareMap.get(DcMotor.class, "motorArm");
-        claw1 = hardwareMap.get(Servo.class, "claw1");
-        claw2 = hardwareMap.get(Servo.class, "claw2");
-
+        robot.init();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            motorLeft.setPower(-0.5);
-            motorRight.setPower(0.5);
+            robot.setDrivePower(0.5, 0.5);
             sleep(100);
-            motorLeft.setPower(0);
-            motorRight.setPower(0);
-            motorArm.setPower(-0.5);
+            robot.setDrivePower(0, 0);
+            robot.setArmPower(0.5);
             sleep(100);
-            motorArm.setPower(0);
-            claw1.setPosition(0.25);
-            claw2.setPosition(0.25);
+            robot.setArmPower(0);
+            robot.setHandPositions(0.25);
             sleep(100);
-            claw1.setPosition(0);
-            claw2.setPosition(0);
-            motorArm.setPower(0.5);
+            robot.setHandPositions(0);
+            robot.setArmPower(-0.5);
             sleep(100);
-            motorArm.setPower(0);
+            robot.setArmPower(0);
         }
     }
 }
