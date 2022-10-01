ab8ada478ef55bc79c5a34aae838f3a5b5012161
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-09-30 19:15:05 -0500

    Updated TurtleTest to use RobotHardware.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtleTest.java
@@ -9,48 +9,35 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="TurtleTestMode", group="Turtle Group")
 public class TurtleTest extends LinearOpMode {
-        DcMotor motorLeft;
-    DcMotor motorRight;
-    DcMotor motorArm;
+    RobotHardware_TT   robot       = new RobotHardware_TT(this);
 
         @Override
         public void runOpMode() {
-            motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-            motorRight = hardwareMap.get(DcMotor.class, "motorRight");
-            motorArm = hardwareMap.get(DcMotor.class, "motorArm");
+            robot.init();
             // Put initialization blocks here
             waitForStart();
             // Put run blocks here
             while (opModeIsActive()) {
 
                 if  (gamepad1.dpad_right) {
-                    motorLeft.setPower(0.5);
-                    motorRight.setPower(-0.5);
+                    robot.setDrivePower(0.5, -0.5);
                 } else if (gamepad1.dpad_left){
-                    motorLeft.setPower(-0.5);
-                    motorRight.setPower(0.5);
+                    robot.setDrivePower(-0.5, 0.5);
                 } else if  (gamepad1.dpad_down) {
-                    motorLeft.setPower(-0.5);
-                    motorRight.setPower(-0.5);
+                    robot.setDrivePower(-0.5, -0.5);
                 } else if (gamepad1.dpad_up){
-                    motorLeft.setPower(0.5);
-                    motorRight.setPower(0.5);
+                    robot.setDrivePower(0.5, 0.5);
                 } else {
-                    motorLeft.setPower(0);
-                    motorRight.setPower(0);
+                    robot.setDrivePower(0, 0);
                 }
 
                 if (gamepad1.left_bumper) {
-                    motorArm.setPower(0.75);
-                    
-                } else {
-                    motorArm.setPower(0);
+                    robot.setArmPower(0.75);
                 }
-               if (gamepad1.right_bumper) {
-                    motorArm.setPower(-0.75);
-
+               else if (gamepad1.right_bumper) {
+                   robot.setArmPower(-0.75);
                 }else {
-                    motorArm.setPower(0);
+                   robot.setArmPower(0);
                 }
             }
         }
