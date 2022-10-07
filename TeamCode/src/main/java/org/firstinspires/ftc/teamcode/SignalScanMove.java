c7a3e798411d67a0c99009913e10d3a7cddffeb3
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-10-07 17:24:41 -0500

    Created SignalScanMove.java to try to put camera and motion together

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalScanMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalScanMove.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
-public class SignalScanMove {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class SignalScanMove extends LinearOpMode {
+    RobotHardware_TT   robot       = new RobotHardware_TT(this);
+
+    @Override
+    public void runOpMode() {
+        robot.init();
+        robot.initCamera();
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+        }
+    }
 }
