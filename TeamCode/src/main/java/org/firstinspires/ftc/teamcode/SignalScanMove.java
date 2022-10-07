c12f8f527e3e40b1f43cf15c9337e8d6ccbdedd2
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-10-07 18:56:25 -0500

    updating SignalScanMove.java to try it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalScanMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalScanMove.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 import java.util.List;
-
+@Autonomous(name="TurtleTestCamera", group="Turtle Group")
 public class SignalScanMove extends LinearOpMode {
     RobotHardware_TT   robot       = new RobotHardware_TT(this);
 
