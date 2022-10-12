47970e8696c65fc4eff3b83a9efa66171ca4a121
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-10-11 19:17:37 -0500

    Made Toggle drive work successfully.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ToggleDrive.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="ToggleDrive", group="Turtle Group")
+public class ToggleDrive extends LinearOpMode {
+    RobotHardware_TT robot = new RobotHardware_TT(this);
+
+    @Override
+    public void runOpMode() {
+        boolean isUsingArcade = false;
+        robot.init();
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.y == true){
+                isUsingArcade = true;
+            }
+            if (gamepad1.x == true) {
+                isUsingArcade = false;
+            }
+
+            if (isUsingArcade == false){
+                robot.setDrivePower(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
+            }
+
+            if (isUsingArcade == true){
+                robot.driveRobot(-gamepad1.left_stick_y, -gamepad1.right_stick_x);
+            }
+        }
+    }
+}
