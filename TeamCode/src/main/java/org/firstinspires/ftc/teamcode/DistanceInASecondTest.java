e217020783dc14b2c3bd35b29a3c630493d396c9
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2022-10-28 19:09:57 -0500

    changed to match

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceInASecondTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceInASecondTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="TurtleTestAutonomous2", group="Turtle Group")
+@Autonomous(name="DistanceInASecondTest", group="Turtle Group")
 public class DistanceInASecondTest extends LinearOpMode{
     RobotHardware_TT   robot       = new RobotHardware_TT(this);
 
