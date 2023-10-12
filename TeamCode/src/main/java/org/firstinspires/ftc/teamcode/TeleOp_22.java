6065aafbcb14cb2eb032511a44fdbb7fd89a4c18
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2023-10-12 17:48:12 -0500

    fixing Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_22.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_22.java
@@ -73,14 +73,14 @@ public class TeleOp_22 extends LinearOpMode {
             if (gamepad2.left_bumper && gamepad2.right_bumper) {
             }
             else if (gamepad2.right_bumper) {
-                servoPositionLeft = servoPositionLeft + 0.01;
-                servoPositionRight = servoPositionRight - 0.01;
+                servoPositionLeft = servoPositionLeft - 0.01;
+                servoPositionRight = servoPositionRight + 0.01;
                 robot.setHandPosition(servoPositionLeft,servoPositionRight);
 
             }
             else if (gamepad2.left_bumper) {
-                servoPositionLeft = 0;
-                servoPositionRight = 1;
+                servoPositionLeft = 1;
+                servoPositionRight = 0;
                 robot.setHandPosition(servoPositionLeft,servoPositionRight);
             }
             //Servos for claw.
